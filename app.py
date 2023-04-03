846d0988e20adf5de9c77b2594bba6e51402cfed
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-03 23:24:05 +0530

    minors

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import webbrowser
 
 def open_in_new_window(url):
-    webbrowser.open_new(url,new=2)
+    webbrowser.open_new(url, new=2)
 
 st.set_page_config(page_title="Open URL in New Window")
 
