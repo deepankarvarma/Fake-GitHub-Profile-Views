bc9e113eb5ed1cb53f0b89b1e648b6ca8d8b5558
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-03 23:16:58 +0530

    changes

--- app.py
+++ app.py
@@ -1,52 +1,10 @@
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+import webbrowser
 
-# Define a function to handle the page reloads
-def reload_page(url, n):
-    # Set up the Chrome webdriver in headless mode
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome()
+def open_in_new_window(url):
+    webbrowser.open_new(url)
 
-    # Load the URL in the browser
-    driver.get(url)
-
-    # Loop through the reloads
-    for i in range(n):
-        try:
-            # Reload the page
-            driver.refresh()
-
-            # Wait for the page to fully load
-            time.sleep(0.2)  # Change the number of seconds as needed
-
-            # Print the title of the page
-            st.write(f"Reload {i+1}: Page Title - {driver.title}")
-        except:
-            st.write(f"An error occurred while reloading page {i+1}. Skipping...")
-
-    # Close the browser
-    driver.quit()
-
-# Set the title of the Streamlit app
-st.title("Page Reloader")
-
-# Add a text input for the URL
-url = st.text_input("Enter the URL to be reloaded")
-
-# Add a number input for the number of reloads
-n = st.number_input("Enter the number of reloads", min_value=1, step=1)
-
-# Add a submit button to start the reloads
-if st.button("Reload"):
-    if not url:
-        st.warning("Please enter a valid URL.")
-    else:
-        try:
-            reload_page(url, n)
-        except:
-            st.error("An error occurred while processing the page reloads. Please try again.")
+st.title("Open URL in New Window")
+url = st.text_input("Enter a URL:")
+if st.button("Open in New Window"):
+    open_in_new_window(url)
