11dcf29199365cddee262eeebe4dcfd6c6b13b99
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-03 23:25:07 +0530

    minors

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import webbrowser
 
-def open_in_new_window(url):
-    webbrowser.open_new(url, new=2)
+def open_in_new_tab(url):
+    webbrowser.open_new_tab(url)
 
-st.set_page_config(page_title="Open URL in New Window")
+st.set_page_config(page_title="Open URL in New Tab")
 
-st.title("Open URL in New Window")
+st.title("Open URL in New Tab")
 url = st.text_input("Enter a URL:")
-if st.button("Open in New Window"):
-    open_in_new_window(url)
+if st.button("Open in New Tab"):
+    open_in_new_tab(url)
